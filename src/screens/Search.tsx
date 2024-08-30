264d0af8c56307413251ba7bd8620b7a288c304e
ChanghyeonO <ckdgus5189@gmail.com>
2024-08-30 17:22:26 +0900

    ✨ 리액트 네이티브 테스트 첫 커밋

---
+++ src/screens/Search.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+const Search = () => {
+    return(
+        <View>
+            <SafeAreaView>
+            <Text>Search Screen</Text>
+            </SafeAreaView>
+        </View>
+    )
+}
+
+export default Search;
